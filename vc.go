8e30f3bfcf2d6edd62f93e6db5833ce4c29ef107
aang <devanshguptamrt@gmail.com>
2019-05-29 17:58:29 +0530

    isvalid method added

--- vc.go
+++ vc.go
@@ -11,6 +11,7 @@ type RequestValidator struct {
 	Params          url.Values
 	RequriredFields []string
 	ParamPatterns   map[string]string
+	state           uint8
 }
 
 //AddFieldPattern, this adds a matching pattern against  a field values
@@ -28,11 +29,13 @@ func (r *RequestValidator) ValidateAgainstPattern() (bool, error) {
 	for k, v := range r.ParamPatterns {
 		for _, x := range r.Params[k] {
 			if ok, err := regexp.MatchString(v, x); !ok {
+				r.state = 0
 				return ok, err
 			}
 
 		}
 	}
+	r.state = 1
 	return true, nil
 }
 
@@ -41,13 +44,21 @@ func (r *RequestValidator) ValidateAgainstPattern() (bool, error) {
 func (r *RequestValidator) HaveRequiredParams() error {
 	for _, v := range r.RequriredFields {
 		if _, ok := r.Params[v]; !ok {
+			r.state = 0
 			return InvalidInput{"required parameter not required"}
 		}
 	}
+	r.state = 1
 	return nil
 }
 
+//IsValid, returns true input is correct so far
+func (r RequestValidator) IsValid() bool {
+	return r.state == valid
+}
+
 const (
+	valid = uint8(1)
 	//Pattern for unsigned integer
 	Pattern_UNINT  = "^\\d+$"
 	Pattern_Email  = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
